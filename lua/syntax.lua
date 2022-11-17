37a521f2a644236c06d5046152f3d156c20424bc
Max Asnaashari <max.asnaashari@canonical.com>
2022-11-17 00:30:11 +0000

    lua/syntax: Customize coq keybinds
    Signed-off-by: Max Asnaashari <max.asnaashari@canonical.com>

--- lua/syntax.lua
+++ lua/syntax.lua
@@ -24,11 +24,24 @@ require'nvim-treesitter.configs'.setup {
 
 vim.g.coq_settings = {
   ["auto_start"] = 'shut-up',
-  ["limits"] = { ["completion_auto_timeout"] = 0.12  },
+  ["limits"] = { ["completion_auto_timeout"] = 0.15  },
   ["clients"] = {
     ["tree_sitter"] = { ["enabled"] = false },
-  }
+  },
+  ["keymap"] = {
+    ["pre_select"] = true,
+    ["recommended"] = false,
+  },
 }
+
+vim.cmd([[
+ino <silent><expr> <Esc>   pumvisible() ? "\<C-e>" : "\<Esc>"
+ino <silent><expr> <C-c>   pumvisible() ? "\<C-e><C-c>" : "\<C-c>"
+ino <silent><expr> <BS>    pumvisible() ? "\<C-e><BS>"  : "\<BS>"
+ino <silent><expr> <CR>    pumvisible() ? (complete_info().selected == -1 ? "\<C-e><CR>" : "\<C-y>") : "\<CR>"
+ino <silent><expr> <Tab>   pumvisible() ? "\<C-n>" : "\<Tab>"
+ino <silent><expr> <S-Tab> pumvisible() ? "\<C-p>" : "\<BS>"
+]])
 
 local coq = require "coq"
 
