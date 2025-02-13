5dcd3eccef6fea33b54dbdd8e5e644c52beec97e
Max Asnaashari <max.asna@futurfusion.io>
2025-02-13 07:43:09 +0000

    lua: Fix cmp toggling
    Signed-off-by: Max Asnaashari <max.asna@futurfusion.io>

--- lua/syntax.lua
+++ lua/syntax.lua
@@ -34,7 +34,13 @@ cmp.setup({
   mapping = cmp.mapping.preset.insert({
     ['<C-d>'] = cmp.mapping.scroll_docs(-4),
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-Space>'] = cmp.sync(function()
+      if cmp.visible() then
+        cmp.abort()
+      else
+        cmp.complete()
+      end
+    end),
     ['<C-e>'] = cmp.mapping.abort(),
     ['<CR>'] = cmp.mapping.confirm({ select = true }),
   }),
