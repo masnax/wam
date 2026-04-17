3fe96c731fb2ee2a8cc1cdc2828eee8fefea2ddb
Max Asnaashari <max@masnax.org>
2026-04-17 14:21:25 -0700

    lua/ts: Fix grep shortcut
    Signed-off-by: Max Asnaashari <max@masnax.org>

--- lua/ts/config.lua
+++ lua/ts/config.lua
@@ -27,14 +27,14 @@ nnoremap('<Space><Space>', function()
   ts.lsp_references(opts)
 end)
 
-nnoremap(';;', function() ts.live_grep({cwd=ts_utils.buffer_dir(), wrap_results=true}) end)
+nnoremap("''", function() ts.live_grep({cwd=ts_utils.buffer_dir(), wrap_results=true}) end)
 nnoremap('||', function()
   local opts = {hidden=true, default_text = ":file:"}
   opts.entry_maker = test_mock_tags(make_entry.gen_from_file(opts), 1)
   ts.find_files(opts)
 end)
 
-nnoremap('\\\\', function()
+nnoremap(';;', function()
   local opts = { path='%:p:h', _entry_cache = {}, default_text = ":file:", hidden = {file_browser = true, folder_browser = true}, follow_symlinks = true }
   opts.entry_maker = function(local_opts)
     local fb_make_entry = require "telescope._extensions.file_browser.make_entry"
@@ -43,7 +43,7 @@ nnoremap('\\\\', function()
   require'telescope'.extensions.file_browser.file_browser(opts)
 end)
 
-nnoremap("''", function() ts.lsp_document_symbols({wrap_results=true, fname_width=2000, symbol_width=50}) end)
+nnoremap('\\\\', function() ts.lsp_document_symbols({wrap_results=true, fname_width=2000, symbol_width=50}) end)
 
 
 local actions = require('telescope.actions')
